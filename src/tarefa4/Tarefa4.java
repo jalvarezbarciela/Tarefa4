568a29e261cd1c3a302512cdc9e37ce0deeb22cf
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2020-05-09 15:31:09 +0200

    Commit 1 Tarefa 4

---
+++ src/tarefa4/Tarefa4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarefa4;
+
+/**
+ *
+ * @author usuario
+ */
+public class Tarefa4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana vent = new Ventana();
+        vent.setVisible(true);
+    }
+    
+}
